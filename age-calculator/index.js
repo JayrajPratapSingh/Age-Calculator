f22e157e4a0d2a134d3a335f71faca628aad4b4c
JayrajPratapSingh <jairaj273310@gmail.com>
2022-11-30 18:33:52 +0530

    uploading my age-calculator app on github

---
+++ age-calculator/index.js
@@ -0,0 +1,34 @@
+
+
+const calculatedage = () =>{
+    const dateEle = document.getElementById("date").value
+    const monthEle = document.getElementById("month").value
+    const yearEle = document.getElementById("year").value
+    
+    
+    let date = new Date();
+    let newDate = date.getDate();
+    let newMonth = 1 + date.getMonth();
+    let newYear =  date.getFullYear();
+    let month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30 , 31]
+
+if(dateEle>newDate){
+    newDate = newDate + month[newMonth-1];
+    newMonth = newMonth-1
+}
+if(monthEle>newMonth){
+    newMonth = newMonth + 12
+    newYear = newYear-1
+}
+let dates = newDate - dateEle;
+let months = newMonth - monthEle;
+let years = newYear - yearEle;
+
+document.getElementById("output").innerHTML = `Your age is ${years} years ${months} months ${dates} days`
+
+}
+
+
+
+
+
